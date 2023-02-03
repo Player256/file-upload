5ef24cb1077f5e4166fbfa8168b296adb23b4ab0
Player255 <dattatreya.work@gmail.com>
2023-02-04 00:15:17 +0530

    video-upload to be added

---
+++ server/models/postMessage.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  message: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+var PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
